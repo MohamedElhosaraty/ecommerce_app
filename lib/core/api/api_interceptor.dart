d787fcebca5b2b1ec10314f6d8a74b403536aa94
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-07-17 00:42:03 +0300

    first comment

---
+++ lib/core/api/api_interceptor.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/cache/cache_helper.dart';
+import 'package:ecommerce_app/core/api/end_Points.dart';
+
+
+class ApiInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    options.headers[ApiKey.token] = CacheHelper.getData(key: ApiKey.token);
+    options.headers["lang"] = "en";
+
+    super.onRequest(options, handler);
+  }
+}
---
+++ lib/shared/components/color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const Color colorBlue = Color(0xff40BFFF);
+
+const Color colorText = Color(0xff223263);
+
+const Color colorGrey = Color(0xff9098B1);
+
+const Color colorTitle = Color(0xff223263);
