18fd2d8e9303d4402fa84bd0307278fa041f512a
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-07-18 16:22:45 +0300

    finish loginScreen

---
+++ lib/cubit/bottom_bar/bottom_cubit.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerce_app/modules/account.dart';
+import 'package:ecommerce_app/modules/cart.dart';
+import 'package:ecommerce_app/modules/home.dart';
+import 'package:ecommerce_app/modules/offer.dart';
+import 'package:ecommerce_app/modules/search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'bottom_state.dart';
+
+class BottomCubit extends Cubit<BottomState> {
+  BottomCubit() : super(BottomInitial());
+
+  static BottomCubit get(context) => BlocProvider.of(context);
+
+  int currentIndex = 0 ;
+
+  List<Widget> screen =
+    const  [
+      HomeScreen(),
+      SearchScreen(),
+      CartScreen(),
+      OfferScreen(),
+      AccountScreen(),
+      ];
+
+  void changeBottom (int index) {
+    currentIndex = index ;
+    emit(BottomChangeNavBarInitial());
+  }
+
+}
